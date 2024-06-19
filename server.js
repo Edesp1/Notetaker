5648208c23792ceb6b3fb1696825b19f162f5651
unknown <edesp.lopez17@gmail.com>
2024-06-19 12:27:02 -0700

    added the starter code provided and put basic dependencies/port on server.js

---
+++ server.js
@@ -0,0 +1,7 @@
+//calls required dependencies and routes needed for the app
+const express = require('express');
+
+
+//defines the app and makes a port to 3001
+const app = express();
+const PORT = process.env.PORT || 3001; 
